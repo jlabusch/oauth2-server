6889da17fda1a686d4d86c8038f49479eff5d6ab
Jacques Labuschagne <jlabusch@acm.org>
2014-06-24 22:08:24 +1200

    Cosmetic improvements.
     * Made num_workers configurable
     * Fixed typos in Blackhole storage class
     * Added backoff algorithm to worker respawns
     * Made Redis/Postgres config more consistent

--- src/server-master.js
+++ src/server-master.js
@@ -13,9 +13,6 @@ exports.run = function(){
         }
     });
 
-    var CPUS = require('os').cpus().length,
-        WORKERS_PER_CPU = CPUS > 1 ? 1 : 2;
-
     config.on('need_reload', function(){
         for (var id in cluster.workers){
             cluster.workers[id].send('reload');
@@ -32,12 +29,33 @@ exports.run = function(){
         return w;
     }
 
-    for (var i = 0; i < CPUS*WORKERS_PER_CPU; ++i){
-        spawn_child();
-    }
+    config.defer(function(err, conf){
+        var n = parseInt(conf.auth_server.num_workers);
+        if (!n || isNaN(n)){
+            n = require('os').cpus().length;
+        }
+        for (var i = 0; i < n; ++i){
+            spawn_child();
+        }
+    });
+
+    var backoff = new (function(){
+        var self = this;
+        this.__last = 0;
+        this.update = function(){ self.__last = (new Date()).getTime(); };
+        this.delay  = function(){
+            var since_last = (new Date()).getTime() - self.__last;
+            if (since_last > 8000){ return 0; }
+            if (since_last > 4000){ return 250; }
+            if (since_last > 2000){ return 500; }
+            if (since_last > 1000){ return 1000; }
+            return 2000;
+        };
+    })();
 
     cluster.on('exit', function(worker, code, signal){
         log('warn', 'worker PID ' + worker.process.pid + ' died (' + (signal || code) + ')');
-        spawn_child();
+        setTimeout(spawn_child, backoff.delay());
+        backoff.update();
     });
 }
